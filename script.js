a0294c69c8d1207c668bdd7118ea400b20f969f2
Clinton <clinton.admpro29@gmail.com>
2025-07-30 21:06:41 -0300

    ...

--- script.js
+++ script.js
@@ -15,3 +15,34 @@ const questions = document.querySelectorAll('.faq-question');
       }
     });
 });
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const items = document.querySelectorAll('.diferencial-item');
+    items.forEach((item, index) => {
+      item.style.opacity = 0;
+      item.style.transform = 'translateY(20px)';
+      setTimeout(() => {
+        item.style.transition = 'all 0.5s ease';
+        item.style.opacity = 1;
+        item.style.transform = 'translateY(0)';
+      }, 150 * index);
+    });
+});
+
+
+ const fotos = document.querySelectorAll('.foto-item img');
+        const lightbox = document.getElementById('lightbox');
+        const lightboxImg = document.getElementById('lightbox-img');
+
+        fotos.forEach(img => {
+        img.addEventListener('click', () => {
+            lightboxImg.src = img.src;
+            lightbox.style.display = 'flex';
+        });
+        });
+
+        function closeLightbox() {
+        lightbox.style.display = 'none';
+        lightboxImg.src = '';
+        }
